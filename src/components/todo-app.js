47313eedae554428f9af7da14bffb5c1a369f556
J. Lopez C <javlc.dev@gmail.com>
2016-05-03 11:52:19 -0400

    0.0.4 Add Delete functionality in the front end.

--- src/components/todo-app.js
+++ src/components/todo-app.js
@@ -23,6 +23,7 @@ class TodoApp extends React.Component {
                 todos={this.state.todos}
                 toggleTask={this.toggleTask.bind(this)}
                 saveTask={this.saveTask.bind(this)}
+                deleteTask={this.deleteTask.bind(this)}
             />
         </div>
     );
@@ -45,6 +46,11 @@ class TodoApp extends React.Component {
   saveTask(oldTask, newTask) {
     const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
     foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
+  deleteTask(taskToDelete) {
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
     this.setState({ todos: this.state.todos });
   }
 }
